015104b8fb8526c502408bfc2bb6b2b2b4281f30
Sean Moore <sean.m.moore@gmail.com>
2021-05-07 10:44:40 -0700

    Implement part 2

--- not-quite-lisp.js
+++ not-quite-lisp.js
@@ -15,11 +15,35 @@ const findFloor = (directions) => {
     }, 0);
 };
 
+// A return value of n >= 1 is the first position of the direction that lead
+// to reaching the target floor. n === 0 means that the floor was reached
+// without any directions (is the starting floor). n < 0 means the floor was
+// never reached.
+const findPosition = (directions, floor) => {
+    let currentFloor = 0;
+    for (let i = 0; i < directions.length; i++) {
+        const direction = directions[i];
+        if (direction === '(') {
+            currentFloor += 1;
+        } else if (direction === ')') {
+            currentFloor -= 1;
+        } else {
+            throw new SyntaxError(`${direction} is not a valid direction`);
+        }
+        if (currentFloor === floor) {
+            return i + 1;
+        }
+    }
+    return -1;
+};
+
 const solution = (filename) => {
     const input = fs.readFileSync(filename, { encoding: 'utf-8' });
-    console.log(findFloor(input));
+    console.log('part 1', findFloor(input));
+    console.log('part 2', findPosition(input, -1));
 };
 
 solution('./input.txt');
 
 exports.findFloor = findFloor;
+exports.findPosition = findPosition;
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { findFloor } = require('./not-quite-lisp');
+const { findFloor, findPosition } = require('./not-quite-lisp');
 
 [
     ['(())', 0],
@@ -10,7 +10,16 @@ const { findFloor } = require('./not-quite-lisp');
     [')))', -3],
     [')())())', -3],
 ].map(([input, expected]) => {
-    test(`findFloor parses ${input} to floor(${expected})`, () => {
+    test(`findFloor parses ${input} to floor ${expected}`, () => {
         expect(findFloor(input)).toBe(expected);
+    });
+});
+
+[
+    [')', -1, 1],
+    ['()())', -1, 5],
+].map(([input, floor, expected]) => {
+    test(`findPosition parses ${input} for floor ${floor} to ${expected}`, () => {
+        expect(findPosition(input, floor)).toBe(expected);
     });
 });
